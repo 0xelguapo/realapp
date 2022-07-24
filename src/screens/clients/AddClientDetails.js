5dc6395250e50c89d314058a9ff4d6398ba4c583
Eric <84822642+erichen114@users.noreply.github.com>
2022-07-24 00:28:40 -0700

    add modal for client details

--- src/screens/clients/AddClientDetails.js
+++ src/screens/clients/AddClientDetails.js
@@ -1,24 +1,99 @@
-import { StyleSheet, View, Text, Animated, Pressable, Button } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Animated,
+  Pressable,
+  Button,
+  TextInput,
+} from "react-native";
+import { useState, useRef, useEffect } from "react";
+import { AntDesign } from "@expo/vector-icons";
 
-export default function AddClientDetails() {
+export default function AddClientDetails(props) {
+  const [title, setTitle] = useState(null);
+  const [description, setDescription] = useState(null);
+  const backdropAnimation = useRef(new Animated.Value(0)).current
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    Animated.timing(backdropAnimation, {
+      toValue: 1,
+      duration: 1000
+    }).start()
+  }, [backdropAnimation])
+
   return (
-    <View style={styles.container}>
+    <Animated.View style={styles.container} >
       <View style={styles.backdrop}></View>
-      <View style={styles.inputsContainer}>
-        <Text>Hi</Text>
+      <View style={styles.modalContainer}>
+        <View style={styles.inputsContainer}>
+          <View style={styles.titleHeaderContainer}>
+            <Text style={styles.titleHeader}>LOG A CONNECTION</Text>
+            <Pressable onPress={props.navigation.goBack}>
+              <AntDesign name="close" size={24} color="#ababab" />
+            </Pressable>
+          </View>
+          <TextInput
+            style={styles.titleInput}
+            value={title}
+            onChangeText={setTitle}
+            ref={inputRef}
+            placeholder="Reached, Left Voicemail, Sent Email..."
+            placeholderTextColor="#d6d6d6"
+          />
+          <TextInput
+            style={styles.descriptionInput}
+            value={description}
+            onChangeText={setDescription}
+            placeholder="Optional Description..."
+            placeholderTextColor="#d6d6d6"
+          />
+        </View>
       </View>
-    </View>
+    </Animated.View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    height: '100%'
+    display: "flex",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, .3)",
   },
   backdrop: {
-    backgroundColor: 'black',
-    opacity: 0.15,
-    height: 300
-  }
+    height: "40%",
+  },
+  modalContainer: {
+    backgroundColor: "white",
+    height: "60%",
+    paddingHorizontal: 15,
+    paddingTop: 15,
+    borderRadius: 18,
+    zIndex: 3,
+  },
+  titleHeaderContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  titleHeader: {
+    fontSize: 13,
+    fontWeight: "500",
+    color: "#ababab",
+    letterSpacing: 1.8,
+    paddingVertical: 5,
+  },
+  titleInput: {
+    height: 40,
+    fontSize: 15,
+  },
+  descriptionInput: {
+    height: 40,
+    fontSize: 15,
+  },
 });
