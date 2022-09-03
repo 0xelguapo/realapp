ee9dd9936da3d6a0f8352b6ef733e2ae98567276
Eric <84822642+erichen114@users.noreply.github.com>
2022-09-02 22:44:16 -0700

    fix add animatinos

--- src/screens/home/Add.js
+++ src/screens/home/Add.js
@@ -1,10 +1,33 @@
-import { Text, View, StyleSheet, Pressable, Animated } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Pressable,
+  Animated,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { useRef, useEffect } from "react";
 
 export default function AddOptions(props) {
   const { goBack } = props.navigation;
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      useNativeDriver: true,
+      toValue: 1,
+      delay: 250,
+      duration: 400,
+    }).start();
+  }, [fadeAnim]);
+
   return (
     <View style={styles.container}>
-      <Pressable onPress={goBack} style={styles.backdrop}></Pressable>
+      <TouchableWithoutFeedback onPress={goBack}>
+        <Animated.View
+          style={{ ...styles.backdrop, opacity: fadeAnim }}
+        ></Animated.View>
+      </TouchableWithoutFeedback>
       <View style={styles.modalContainer}></View>
     </View>
   );
@@ -13,15 +36,18 @@ export default function AddOptions(props) {
 const styles = StyleSheet.create({
   container: {
     display: "flex",
+    flexDirection: "column-reverse",
     height: "100%",
-    backgroundColor: "rgba(0, 0, 0, .3)",
   },
   backdrop: {
-    flex: 1,
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+    backgroundColor: "rgba(0, 0, 0, .3)",
   },
   modalContainer: {
     display: "flex",
-    flexGrow: 1,
+    height: 400,
     backgroundColor: "white",
     paddingHorizontal: 18,
     paddingVertical: 15,
